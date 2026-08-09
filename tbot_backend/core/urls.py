f1abd5830faf6a035f639e6d13f885cb7c1574a0
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2026-08-09 15:45:26 -0400

    error fix hopefully

--- tbot_backend/core/urls.py
+++ tbot_backend/core/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+from django.views import View
+
+
+class ApiRootView(View):
+    http_method_names = ["get"]
+
+    def get(self, _request):
+        return JsonResponse(
+            {
+                "status": "ok",
+                "endpoints": [
+                    "/tbotapp/decklists/",
+                    "/tbotapp/cardinformation/",
+                ],
+            }
+        )
 
 urlpatterns = [
+    path('', ApiRootView.as_view(), name='api_root'),
     path('admin/', admin.site.urls),
     path('tbotapp/', include('tbotapp.urls')),
 ]
