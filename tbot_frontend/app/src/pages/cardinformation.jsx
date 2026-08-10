b5f70652f2e73a46ca730d7fd247dd637a6640b8
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2026-08-10 15:40:16 -0400

    changed sneaky icon back

--- tbot_frontend/app/src/pages/cardinformation.jsx
+++ tbot_frontend/app/src/pages/cardinformation.jsx
@@ -69,7 +69,7 @@ const CLASS_ICON_LINKS = {
   brainy: "https://i.ibb.co/d40zFh8r/Brainy.webp",
   crazy: "https://i.ibb.co/HTvzSsXX/crazy.webp",
   hearty: "https://i.ibb.co/ynKbzV8v/hearty.webp",
-  sneaky: "https://plantsvszombies.wiki.gg/images/PvZH_Sneaky_Icon.png?c6fd41=&format=original",
+  sneaky: "https://i.ibb.co/nqFdR6HJ/Pv-ZH-Sneaky-Icon.png",
 };
 
 function CardInformation() {
