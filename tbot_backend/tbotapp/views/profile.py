24340d98ae5b21d60dc6bef3f7f2001cec389310
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2026-09-07 21:53:57 -0400

    fixed profile

--- tbot_backend/tbotapp/views/profile.py
+++ tbot_backend/tbotapp/views/profile.py
@@ -102,15 +102,12 @@ def profile_detail(request, profile_slug):
         profile_id=profile.id
     ).count()
 
-    card_count = profile.cards.count()
-
     serializer = UserProfileSerializer(profile)
 
     return Response(
         {
             "profile": serializer.data,
             "deck_count": deck_count,
-            "card_count": card_count,
             "is_owner": is_owner,
             "is_site_owner": is_site_owner,
         },
